a520ec3e29893b311231ca5278bcef73b62b352a
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-06-16 20:42:04 +0000

    🐬

--- wo1/src/app/page.tsx
+++ wo1/src/app/page.tsx
@@ -5,13 +5,7 @@ export default function Home() {
 		<>
 			<header className="flex justify-between items-center mb-4">
 				<h1 className="text-2xl">ToDo's</h1>
-				<Link
-					className="border border-pink-300 text-pink-200 px-2 py-1 rounded hover:bg-pink-700 focus-within:bg-red-500 outline outline-offset-1 outline-1 "
-					href="/new"
-				>
-					New
-				</Link>
-				<Link className="mybutton" href="/new">
+				<Link className="mybutton " href="/new">
 					New
 				</Link>
 			</header>
