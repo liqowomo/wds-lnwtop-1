1f7f5f3a827ac1d0d7217729cd624b8668a2741e
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-06-16 20:49:03 +0000

    🐬

--- wo1/src/app/page.tsx
+++ wo1/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<>
 			<header className="flex justify-between items-center mb-4">
-				<h1 className="text-2xl">ToDos</h1>
+				<h1 className="text-2xl">ToDos and Shizz</h1>
 				<Link className="mybutton " href="/new">
 					New
 				</Link>
