3bb3624f01b070c2a9a1fab92d355ab11e6d0f85
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-06-18 18:24:39 +0000

    🐬

--- wo2/src/app/page.tsx
+++ wo2/src/app/page.tsx
@@ -4,15 +4,19 @@ import Link from 'next/link'
 export default function page() {
 	return (
 		<>
-			<Image
-				className="mx-auto"
-				src={
-					'https://imagecdn.clips4sale.com/accounts99/2582/clip_images/LX%20Workout%20BUSTED.gif'
-				}
-				alt="bbw"
-				width={800}
-				height={200}
-			/>
+			<div className="mx-auto container outline p-2">
+				<Image
+					src={
+						'https://imagecdn.clips4sale.com/accounts99/2582/clip_images/LX%20Workout%20BUSTED.gif'
+					}
+					alt="bbw"
+					width={800}
+					height={200}
+				/>
+				<p className="italic tracking-widest text-center p-2 outline">
+					"SNIFFFFFFFFF"
+				</p>
+			</div>
 			<h1>Pussy</h1>
 		</>
 	)
