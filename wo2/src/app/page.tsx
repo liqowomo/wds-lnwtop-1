60e6d4f32839ae7722c42f9a39f2c490714ce990
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-06-18 22:35:46 +0000

    🐬

--- wo2/src/app/page.tsx
+++ wo2/src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function page() {
 	return (
 		<>
-			<div className="mx-auto flex container outline p-2 justify-center">
+			<div className="mx-auto flex flex-col outline p-2 justify-center items-center">
 				<Image
 					src={'https://media.tenor.com/PmJMkdZKA20AAAAC/dance-dancing.gif'}
 					alt="bbw"
@@ -16,7 +16,12 @@ export default function page() {
 					&lsquo; Dance &lsquo;
 				</p>
 			</div>
-			<h1>Pussy</h1>
+			<div className="flex justify-between items-center mb-4 py-5 outline p-5">
+				<h1>Pussy</h1>
+				<Link className="mybutton" href="/new">
+					New
+				</Link>
+			</div>
 		</>
 	)
 }
