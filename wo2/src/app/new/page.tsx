4140bc5c61ee84786637a2c00a0a2c148f9617c0
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-06-19 21:36:18 +0000

    🐬

--- wo2/src/app/new/page.tsx
+++ wo2/src/app/new/page.tsx
@@ -1,3 +1,26 @@
-export default function page() {
-	return <div>New page Bastard</div>
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Page() {
+	return (
+		<>
+			<header className="flex justify-between items-center mb-4">
+				<h1 className="text-2xl">Boobs </h1>
+			</header>
+			<form className="flex gap-2 flex-col">
+				<input
+					type="text"
+					placeholder="title"
+					className="border border-blue-300 bg-transparent rounded px-2 py-1 focus-within:border-teal-700"
+				/>
+				<div className="flex gap-1 justify-end">
+					{/* Note that .. means to go back  */}
+					<Link href="..">
+						<button className="mybutton">Cancel</button>
+						<button className="mybutton">Submit</button>
+					</Link>
+				</div>
+			</form>
+		</>
+	)
 }
--- wo2/src/components/TodoItem.tsx
+++ wo2/src/components/TodoItem.tsx
@@ -13,7 +13,10 @@ export function TodoItem({id, title, Complete}: TodoItemProps) {
 	return (
 		<li className="flex gap-1 items-center">
 			<input id={id} type="checkbox" className="cursor-pointer peer" />
-			<label htmlFor={id} className="peer-checked:line-through">
+			<label
+				htmlFor={id}
+				className="cursor-pointer peer-checked:line-through peer-checked:text-blue-900"
+			>
 				{title}
 			</label>
 		</li>
